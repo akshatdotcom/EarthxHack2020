a97e96c0eddd68c0642635ccbc5ec7bb133f8d7e
Akshat Shah <aksshah2004@gmail.com>
2020-04-26 18:20:55 -0500

    replaced ^3 with real exponent

--- Flutter/lib/home.dart
+++ Flutter/lib/home.dart
@@ -85,7 +85,7 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
             InputContainerHouse("Electricity bill", "Price per kWh", this, 1),
-            InputContainerHouse("Natural gas bill", "Price per 1000 ft^3", this, 2),
+            InputContainerHouse("Natural gas bill", "Price per 1000 ft³", this, 2),
             InputContainerHouse("Miles driven", "MPG", this, 3),
             RaisedButton(
               onPressed: () {
